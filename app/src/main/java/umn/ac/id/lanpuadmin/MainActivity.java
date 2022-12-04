7615eaa4812803e22345bc3871fb8c9591fa6f37
AlfredPros <alfredkuhlman@yahoo.co.id>
2022-12-04 14:28:31 +0700

    QR Reader!

--- app/src/main/java/umn/ac/id/lanpuadmin/MainActivity.java
+++ app/src/main/java/umn/ac/id/lanpuadmin/MainActivity.java
@@ -1,19 +1,29 @@
 package umn.ac.id.lanpuadmin;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     private Button logoutButton;
+    private Button gateInButton;
+    private TextView outputQr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        outputQr = findViewById(R.id.outputQR);
 
         logoutButton = findViewById(R.id.logOutButton);
         logoutButton.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +34,35 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        gateInButton = findViewById(R.id.gateInButton);
+        gateInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setPrompt("Scan a barcode or QR Code");
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        // if the intentResult is null then
+        // toast a message as "cancelled"
+        if (intentResult != null) {
+            if (intentResult.getContents() == null) {
+                Toast.makeText(getBaseContext(), "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                // if the intentResult is not null we'll set
+                // the content and format of scan message
+                outputQr.setText(intentResult.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
